5bcbcff0ffd9c8489c754bf8b1c6de2cce3a9bd0
prabhavgupta <prabhavgupta1996@gmail.com>
2017-09-06 02:31:07 +0530

    Part 1

--- Part 1 - Data Preprocessing/PreprocessTemplate.R
+++ Part 1 - Data Preprocessing/PreprocessTemplate.R
@@ -11,3 +11,27 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                  ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                  dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         level=c('France', 'Spain', 'Germany') ,
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         level=c('No', 'Yes') ,
+                         labels = c(0,1))
+
+# Splitting dataset : Test and Train
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased ,
+                     SplitRatio = 0.8 )
+
+training_set = subset(dataset , split==TRUE)
+test_set = subset(dataset , split==FALSE)
+
+# Feature scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
+
--- Part 1 - Data Preprocessing/PreprocessTemplate.py
+++ Part 1 - Data Preprocessing/PreprocessTemplate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  5 00:15:15 2017
+Created on Tue Sep 5 00:15:15 2017
 
 @author: prabhav
 """
@@ -13,9 +13,28 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
+#Taking care of missing data
 from sklearn.preprocessing import Imputer 
 imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[: , 1:3])
 X[: , 1:3] = imputer.transform(X[: , 1:3])
-print(X)
 
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder ,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0]) #Problem : Since 0<1<2 , it might appear that there is a relational order in countries 
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+
+from sklearn.cross_validation import train_test_split
+X_train , X_test, Y_train , Y_test = train_test_split(X,Y,test_size = 0.2 , random_state = 0 )
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+X_train = sc_x.fit_transform(X_train)
+X_test = sc_x.transform(X_test)
